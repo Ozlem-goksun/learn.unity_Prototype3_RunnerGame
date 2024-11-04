01533445563455adee10e59c1cd098a03f8336da
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-11-04 14:06:35 +0300

    a little update

--- learn.unity_Prototype3_RunnerGame/Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
+++ learn.unity_Prototype3_RunnerGame/Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class SpinObjectsX : MonoBehaviour
 {
     public float spinSpeed;
+    public float jumpSpeed = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+
     }
 }
